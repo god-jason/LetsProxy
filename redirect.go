dfafb12621ac8478b679f4c8931be3515580ec1d
jason <jason@zgwit.com>
2021-02-02 17:52:03 +0800

    测试一下SSH

--- redirect.go
+++ redirect.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
 type redirectHandler struct {}
 func (rh *redirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.URL.Scheme = "https"
-	//log.Println(r.URL.String())
 	http.Redirect(w, r, r.URL.String(), http.StatusSeeOther)
 }
 
